1576b23a77addf5bb201232ba7a79f22dc1ff8ac
NETVINE <vinecodes@gmail.com>
2020-11-16 02:16:23 +0100

    initial commit

---
+++ src/PermissionMethod.java
@@ -0,0 +1,23 @@
+public class PermissionMethod
+{
+    public static void main (String[]args)
+    {
+      System.out.println("hello world");
+      String name = "Vine";
+    if (name== "Vine")
+    {
+        grantPermission();
+    }
+    }
+
+    static void grantPermission()
+        {
+            System.out.println("special permissions");
+        }
+
+
+
+
+
+
+}
